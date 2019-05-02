cefd425868ce0aa8efb8ad2b102670569884c758
corujasdevbr <fernando.guerra@corujasdev.com.br>
2019-05-02 15:30:33 -0300

    up

--- tarde/RNSvigufo/src/pages/main.js
+++ tarde/RNSvigufo/src/pages/main.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 
-import { Text, Image, StyleSheet } from "react-native";
+import { Text, Image, StyleSheet, View, FlatList } from "react-native";
+
+import api from "../services/api";
 
 class Main extends Component {
   static navigationOptions = {
@@ -12,13 +14,153 @@ class Main extends Component {
     )
   };
 
-  render() {
-    return <Text style={{fontSize: 40}}>Main</Text>;
+  constructor(props) {
+    super(props);
+    this.state = {
+      listaEventos: []
+    };
   }
+
+  componentDidMount() {
+    this.carregarEventos();
+  }
+
+  carregarEventos = async () => {
+    const resposta = await api.get("/eventos");
+    const dadosDaApi = resposta.data;
+    this.setState({ listaEventos : dadosDaApi });
+  };
+
+  render() {
+    return (
+      <View style={styles.main}>
+        {/* cabecalho */}
+        <View style={styles.mainHeader}>
+          {/* linha do titulo */}
+          <View style={styles.mainHeaderRow}>
+            <Image
+              source={require("../assets/img/calendar.png")}
+              style={styles.mainHeaderImg}
+            />
+            <Text style={styles.mainHeaderText}>{"Eventos".toUpperCase()}</Text>
+          </View>
+          {/* borda */}
+          <View style={styles.mainHeaderLine} />
+        </View>
+
+        <View style={styles.mainBody}>
+          <FlatList
+            contentContainerStyle={styles.mainBodyConteudo}
+            data={this.state.listaEventos}
+            keyExtractor={item => item.id}
+            renderItem={this.renderizaItem}
+          />
+        </View>
+      </View>
+    );
+  }
+
+  renderizaItem = ({ item }) => (
+    <View style={styles.flatItemLinha}>
+      <View style={styles.flatItemContainer}>
+        <Text style={styles.flatItemTitulo}>{item.titulo}</Text>
+        <Text style={styles.flatItemData}>{item.dataEvento}</Text>
+      </View>
+      <View style={styles.flatItemImg}>
+        <Image
+          source={require("../assets/img/view.png")}
+          style={styles.flatItemImgIcon}
+        />
+      </View>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-  tabNavigatorIconHome: { width: 25, height: 25, tintColor: "#FFFFFF" }
+  tabNavigatorIconHome: {
+    width: 25,
+    height: 25,
+    // tintColor: "purple"
+    tintColor: "#FFFFFF"
+  },
+  // conteúdo da main
+  main: {
+    flex: 1,
+    backgroundColor: "#F1F1F1"
+  },
+  // cabecalho
+  mainHeaderRow: {
+    flexDirection: "row"
+  },
+  mainHeader: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center"
+    // backgroundColor: "red"
+  },
+  // imagem do cabeçalho
+  mainHeaderImg: {
+    width: 22,
+    height: 22,
+    tintColor: "#cccccc",
+    marginRight: -9,
+    marginTop: -9
+  },
+  // texto do cabecalho
+  mainHeaderText: {
+    fontSize: 16,
+    letterSpacing: 5,
+    color: "#999999",
+    fontFamily: "OpenSans-Regular"
+  },
+  // linha de separacao do cabecalho
+  mainHeaderLine: {
+    width: 170,
+    paddingTop: 10,
+    borderBottomColor: "#999999",
+    borderBottomWidth: 0.9
+  },
+  // corpo do texto
+  mainBody: {
+    // backgroundColor: "blue",
+    flex: 4
+  },
+  // conteúdo da lista
+  mainBodyConteudo: {
+    paddingTop: 30,
+    paddingRight: 50,
+    paddingLeft: 50
+  },
+  // dados do evento de cada item da linha
+  flatItemLinha: {
+    flexDirection: "row",
+    borderBottomWidth: 0.9,
+    borderBottomColor: "gray"
+  },
+  flatItemContainer: {
+    flex: 7,
+    marginTop: 5
+  },
+  flatItemTitulo: {
+    fontSize: 14,
+    color: "#333",
+    fontFamily: "OpenSans-Light"
+  },
+  flatItemData: {
+    fontSize: 10,
+    color: "#999",
+    lineHeight: 24
+  },
+  flatItemImg: {
+    justifyContent: "center",
+    alignContent: "center",
+    alignItems: "center"
+  },
+  flatItemImgIcon: {
+    width: 22,
+    height: 22,
+    tintColor: "#B727FF"
+  }
 });
 
 export default Main;
